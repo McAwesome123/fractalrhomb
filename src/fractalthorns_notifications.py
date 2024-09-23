ac4103c81c93b1e3f4ca8cb50b8806825d020c5d
pierce-smith1 <pierce.smith@rezstream.com>
2024-09-23 01:22:53 -0600

    add some failure tolerance and retry logic

--- src/fractalthorns_notifications.py
+++ src/fractalthorns_notifications.py
@@ -8,25 +8,46 @@
 
 """Code responsible for listening to and relaying SSE notifications."""
 
+import asyncio
 import json
 import logging
 import re
 from os import getenv
+from datetime import timedelta
 
 from aiohttp_sse_client2 import client as sse_client
 
 from src.fractalrhomb_globals import bot
 
-notifs_logger = logging.getLogger("notifs")
+notifs_logger = logging.getLogger("discord")
+
+BASE_RETRY_INTERVAL = timedelta(seconds=30)
+MAX_RETRY_INTERVAL = timedelta(hours=1)
 
 async def listen_for_notifications() -> None:
-    async with sse_client.EventSource('https://fractalthorns.com/notifications') as event_source:
-        try:
-            async for event in event_source:
-                await handle_notification(event)
-        except ConnectionError:
-            pass
+    retry_interval = BASE_RETRY_INTERVAL
 
+    while True:
+        notifs_logger.info(f'trying to connect to sse endpoint')
+        try:
+            async with sse_client.EventSource('https://fractalthorns.com/notifications') as event_source:
+                async for event in event_source:
+                    await handle_notification(event)
+                    retry_interval = BASE_RETRY_INTERVAL
+
+        except Exception:
+            # This SSE client does have its own retry logic, but it will only 
+            # retry on certain very specific failures.
+            # If the server 503s, for example, it will permanently give up.
+            # If it can't get a connection for any reason, I still want the bot 
+            # to continually retry no matter what so that it doesn't have to be
+            # restarted, so we need a little bit of custom retry logic.
+            notifs_logger.warning(f'couldn\'t connect to the sse server, trying again in {retry_interval.total_seconds()} seconds')
+            await asyncio.sleep(retry_interval.total_seconds())
+
+            retry_interval *= 2
+            retry_interval = MAX_RETRY_INTERVAL if retry_interval > MAX_RETRY_INTERVAL else retry_interval
+        
 async def handle_notification(notification):
     notifs_logger.info(f'caught a sse notification: {notification}')
     body = notification.data
