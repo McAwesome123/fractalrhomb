1cb0f652ba5a75df1b1f6de7810707c87c88464b
McAwesome <49949995+McAwesome123@users.noreply.github.com>
2025-05-11 15:54:02 +0300

    Version bump

--- src/fractalrhomb_globals.py
+++ src/fractalrhomb_globals.py
@@ -53,9 +53,9 @@ NO_ITEMS_MATCH_SEARCH = "no items match the requested parameters"
 INTERACTION_TOO_MANY_FOLLOW_UP_MESSAGES_ERROR_CODE = 40094
 
 # The full version number including anything extra.
-FRACTALRHOMB_VERSION_FULL = "0.9.0"
+FRACTALRHOMB_VERSION_FULL = "0.9.1"
 # Version number with only Major, Minor, and Patch version.
-FRACTALRHOMB_VERSION_LONG = "0.9.0"
+FRACTALRHOMB_VERSION_LONG = "0.9.1"
 # Verison number with only Major and Minor version.
 FRACTALRHOMB_VERSION_SHORT = "0.9"
 
